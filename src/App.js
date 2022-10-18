b0679b5e8dd2d56c4e7c0e12fe4ecadb44735645
Catalin Chitu <catalin.george.chitu@gmail.com>
2022-10-18 17:03:47 +0300

    Created the folder structure for the project

--- src/App.js
+++ src/App.js
@@ -1,25 +1,24 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+	return (
+		<div>
+			<div className='h-screen w-full snap-y snap-mandatory overflow-y-scroll bg-red-400 '>
+				<div className='flex h-screen w-full snap-start items-center justify-center bg-blue-400 text-6xl'>
+					1
+				</div>
+				<div className='flex h-screen w-full snap-start items-center justify-center bg-yellow-400 text-6xl'>
+					2
+				</div>
+				<div className='flex h-screen w-full snap-start items-center justify-center bg-green-400 text-6xl'>
+					3
+				</div>
+				<div className='flex h-screen w-full snap-start items-center justify-center bg-amber-600 text-6xl'>
+					4
+				</div>
+			</div>
+		</div>
+	);
 }
 
 export default App;
---
+++ src/components/Navbar/Navbar.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+export default function () {
+	return (
+		<nav className='navbar'>
+			<div className='navbar-logo'></div>
+			<ul>
+				<li>about</li>
+				<li>portofolio</li>
+				<li>cv</li>
+				<li>contact</li>
+				<li>github</li>
+			</ul>
+		</nav>
+	);
+}
